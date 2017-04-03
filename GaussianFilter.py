4d3a9cad73a2f85963d37a68f7b2acd8d9aa4026
Margarita <margo.himera@yandex.ru>
2017-04-04 00:15:13 +0300

    Gaussian filter is ready

--- GaussianFilter.py
+++ GaussianFilter.py
@@ -2,65 +2,64 @@
 
 from PyQt5.QtGui import QImage, qRgb, QColor
 from math import pi, exp, sqrt
-import numpy as np
 
-# вычисление матрицы Гаусса
-def getMatrix(R, sigma):
-    global matrix, horizontal, vertical
+# Модуль для размытия по Гауссу
+# Используется сепарабельность Гауссова размытия
+
+# вычисляем вектор по формуле одномерного Гауссова распределения 
+def getVector(R, sigma):
+    global vector
     matrix_size = 2*R + 1
-    matrix = [[0 for x in range(matrix_size)] for y in range(matrix_size)]
-    horizontal = [0 for x in range(matrix_size)]
+    vector = [0 for x in range(matrix_size)]
+
     total_sum = 0
-    C = 1 / (2*pi*sigma**2)
     divider = 2*sigma**2
-    # вычисляем элементы матрицы по формуле и находим сумму всех ее элементов
-    for i in range(-R, R + 1):
-        for j in range(-R, R + 1):
-            matrix[i + R][j + R] = C * exp(-(i**2 + j**2) / divider) 
-            total_sum += matrix[i + R][j + R]
-
-    mult = 1 / total_sum
-    # домножаем элементы матрицы, чтобы сумма элементов была равна 1
-    for i in range(matrix_size):
-        for j in range(matrix_size):
-            matrix[i][j] *= mult
-
-    # вычисляем два вектора по формуле одномерного Гауссова распределения 
-    total_sum = 0
     C = 1 / (sqrt(2*pi)*sigma)
     for i in range(-R, R + 1):
-        horizontal[i + R] = C * exp(-(i**2) / divider)
-        total_sum += horizontal[i + R]
+        vector[i + R] = C * exp(-(i**2) / divider)
+        total_sum += vector[i + R]
+    # нормализуем значения вектора
     for i in range(-R, R + 1):
-        horizontal[i + R] /= total_sum
-    vertical = np.vstack(horizontal)
+        vector[i + R] /= total_sum
 
 # применение фильтра Гаусса к изображению
 def solve(R, sigma, image):
-    getMatrix(R, sigma)
-    for i in range(image.width()):
-        for j in range(image.height()):
-            changePixelColor(image, i, j, R)
-
-# изменение цвета пикселя средним значением соседних пикселей
-def changePixelColor(image, x, y, R):
-    result = QColor()
+    getVector(R, sigma)
     width = image.width()
     height = image.height()
+    # первый проход - горизонтальное размытие
+    for j in range(height):
+        for i in range(width):
+            changePixelColor(image, i, j, True, R)
+    # второй проход - вертикальное размытие
+    for i in range(width):
+        for j in range(height):
+            changePixelColor(image, i, j, False, R)
+
+# изменение цвета пикселя средним значением соседних пикселей
+# isHorizontal = True, если мы движемся по горизонтали, иначе False
+def changePixelColor(image, x, y, isHorizontal, R):
+    result = QColor()
     for i in range(-R, R + 1):
-        for j in range (-R, R + 1):
-            # берем текущий пиксель, если координаты выходят за размеры изображения
-            x_current = x + i
-            y_current = y + j
-            if (x_current < 0 or x_current >= width):
+        x_current = x + i
+        y_current = y + i
+        # проверяем правильность координат в зависимости от направления движения
+        if isHorizontal:
+            # при движении по горизонтали y не меняется
+            y_current = y
+            if (x_current < 0 or x_current >= image.width()):
                 x_current = x
-            if (y_current < 0 or y_current >= height):
+        else:
+            # при движении по вертикали x не меняется
+            x_current = x
+            if (y_current < 0 or y_current >= image.height()):
                 y_current = y
-            # берем цвет текущего пикселя и домножаем его компоненты на элемент матрицы
-            pixel = image.pixel(x_current, y_current)
-            color = QColor(pixel)
-            changeColor(color, matrix[i][j]) 
-            addColor(result, color)
+        # берем цвет текущего пикселя и домножаем его компоненты на элемент матрицы
+        pixel = image.pixel(x_current, y_current)
+        color = QColor(pixel)
+        changeColor(color, vector[i + R])
+        addColor(result, color)
+    # сохраняем полученный цвет в изображение
     image.setPixelColor(x, y, result)
 
 # изменение значения цвета путем умножения его компонент на заданный коэффициент
