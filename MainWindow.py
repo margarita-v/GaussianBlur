7a9eb9e5bc0a22f6a9816fb521178c04a2376497
Margarita <margo.himera@yandex.ru>
2017-04-04 19:12:55 +0300

    Refactoring

--- MainWindow.py
+++ MainWindow.py
@@ -6,9 +6,15 @@ from PyQt5 import uic
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt, QDir, QEvent
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QFileDialog, 
-        QLabel, QMessageBox, QInputDialog, QDialog)
-import GaussianFilter
+        QLabel, QMessageBox, QDialog)
 from Dialog import InputDialog
+import GaussianFilter
+import os
+
+pictures_folder = 'pictures'
+result_folder = 'result'
+current_dir = QDir.currentPath()
+image_filter = 'Images (*.png *.jpg)'
 
 class Window(QMainWindow):
 
@@ -21,37 +27,32 @@ class Window(QMainWindow):
         
         self.lblSecondImage.setVisible(False)
         self.actionRun.triggered.connect(self.getSolve)
-        self.actionRun.setShortcut('Ctrl+R')
         self.actionOpen.triggered.connect(self.openDialog)
-        self.actionOpen.setShortcut('Ctrl+O')
         self.actionSave.triggered.connect(self.saveDialog)
-        self.actionSave.setShortcut('Ctrl+S')
+        self.actionExit.triggered.connect(self.exit)
 
         self.show()
     
-    # выход из программы по нажатию esc
-    def keyPressEvent(self, e):
-        if e.key() == Qt.Key_Escape:
-            self.close()
-
     # диалог для открытия изображения
     def openDialog(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Open file", QDir.currentPath(), \
-                "Images (*.png *.jpg)")
+        filename, _ = QFileDialog.getOpenFileName(self, "Open file", 
+                os.path.join(current_dir, pictures_folder), image_filter)
         if filename:
-            self.menuTask.setEnabled(True)
             self.image = QImage(filename)
             self.resultImage = None
             if self.image.isNull():
-                QMessageBox.information(self, "Image Viewer",
-                        "Cannot load %s." % fileName)
+                QMessageBox.critical(self, "Image Viewer", "Cannot load %s." % fileName)
                 return
+            
+            self.menuTask.setEnabled(True)
+            self.actionRun.setEnabled(True)
+            self.actionSave.setEnabled(False)
 
             self.lblSecondImage.setVisible(False)
             self.pixmap = QPixmap.fromImage(self.image)
             # отображаем изображение, сохраняя его пропорции, не теряя качества
-            self.scaledPixmap = self.pixmap.scaled(self.lblFirstImage.size(), \
-                    Qt.KeepAspectRatio, \
+            self.scaledPixmap = self.pixmap.scaled(self.lblFirstImage.size(), 
+                    Qt.KeepAspectRatio, 
                     Qt.SmoothTransformation)
             self.lblFirstImage.setPixmap(self.scaledPixmap)
             # событие, которое вызывается при масштабировании изображения
@@ -60,7 +61,12 @@ class Window(QMainWindow):
    
     # диалог для сохранения изображения, полученного после применения алгоритма
     def saveDialog(self):
-        return
+        filename, _ = QFileDialog.getSaveFileName(self, "Save file", 
+                os.path.join(current_dir, result_folder), image_filter)
+        if filename:
+            if not self.resultImage.save(filename):
+                QMessageBox.critical(self, "Save image", 
+                        "Please, enter a file extension (.jpg or .png)!")
 
     # событие изменения размера изображения
     def eventFilter(self, source, event):
@@ -73,8 +79,8 @@ class Window(QMainWindow):
             pixmap = QPixmap.fromImage(self.resultImage)
         else:
             return super(Window, self).eventFilter(source, event)
-        source.setPixmap(pixmap.scaled(source.size(), \
-                Qt.KeepAspectRatio, \
+        source.setPixmap(pixmap.scaled(source.size(),
+                Qt.KeepAspectRatio,
                 Qt.SmoothTransformation))
         return super(Window, self).eventFilter(source, event)
    
@@ -88,12 +94,20 @@ class Window(QMainWindow):
             # передаем изображение по ссылке и применяем к нему фильтр
             GaussianFilter.solve(dialog.radius, dialog.sigma, self.resultImage)
             pixmap = QPixmap.fromImage(self.resultImage)
-            scaledPixmap = pixmap.scaled(self.lblSecondImage.size(), \
-                    Qt.KeepAspectRatio, \
+            scaledPixmap = pixmap.scaled(self.lblSecondImage.size(),
+                    Qt.KeepAspectRatio,
                     Qt.SmoothTransformation)
             # отображаем новое изображение на компоненте
             self.lblSecondImage.setPixmap(scaledPixmap)
             self.lblSecondImage.setVisible(True)
+            self.actionSave.setEnabled(True)
+
+    # пункт меню Exit
+    def exit(self):
+        result = QMessageBox.question(self, "Confirm Exit",
+				  "Are you sure you want to exit ?")
+        if result == QMessageBox.Yes:
+            self.close()
 
 if __name__ == '__main__':
     import sys
